20407054cb4bf4117da9292ff15ac3785aa791a3
Alexander <3724198@mail.ru>
2023-12-17 23:13:29 +0300

    task4

---
+++ src/modules/searchbar/searchbar.ts
@@ -0,0 +1,6 @@
+import html from './searchbar.tpl.html';
+import { Component } from '../component';
+
+class Searchbar extends Component {}
+
+export const searchBar = new Searchbar(html);
--- src/router.ts
+++ src/router.ts
@@ -3,12 +3,14 @@ import { notFoundComp } from './modules/notFound/notFound';
 import { homepageComp } from './modules/homepage/homepage';
 import { productDetailComp } from './modules/productDetail/productDetail';
 import { checkoutComp } from './modules/checkout/checkout';
+import { searchBar } from './modules/searchbar/searchbar';
 
 const ROUTES = {
   '/': homepageComp,
   '/catalog': catalogComp,
   '/product': productDetailComp,
-  '/checkout': checkoutComp
+  '/checkout': checkoutComp,
+  '/searchbar': searchBar
 };
 
 export default class Router {
